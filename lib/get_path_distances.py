e1bb383894b28354607e9da6efa9d929853aaf48
Shawn Laffan <shawnlaffan@gmail.com>
2012-03-14 17:31:02 +1100

    approach using raster path distances
    Signed-off-by: Shawn Laffan <shawnlaffan@gmail.com>

---
+++ lib/get_path_distances.py
@@ -0,0 +1,91 @@
+"""
+Process data tables collected for Matt Taylor's Mulloway research.
+Determine if they are best BBQ'd or crumbed and oven roasted.
+"""
+
+
+# Import arcpy module and other required modules
+import arcpy
+import arcpy.management as arcmgt
+from arcpy.sa import *
+arcpy.CheckOutExtension("Spatial")
+
+import os
+import string
+import math
+import pprint
+
+def add_msg_and_print(msg, severity=0):
+    # Adds a Message to the geoprocessor (in case this is run as a tool)
+    # and also prints the message to the screen (standard output)
+    # 
+    print msg
+
+    # Split the message on \n first, so that if it's multiple lines, 
+    #  a GPMessage will be added for each line
+    try:
+        for string in msg.split('\n'):
+            # Add appropriate geoprocessing message 
+            #
+            if severity == 0:
+                arcpy.AddMessage(string)
+            elif severity == 1:
+                arcpy.AddWarning(string)
+            elif severity == 2:
+                arcpy.AddError(string)
+    except:
+        pass
+
+
+if __name__ == "__main__":
+
+    in_file   = arcpy.GetParameterAsText (0)
+    out_file  = arcpy.GetParameterAsText (1)
+    cost_rast = arcpy.GetParameterAsText (2)
+    workspace = arcpy.GetParameterAsText (3)
+
+    arcpy.env.overwriteOutput = True
+
+    arcpy.env.workspace = workspace
+    if (arcpy.env.workspace is None):
+        arcpy.env.workspace = os.getcwd()
+    
+    arcpy.env.extent = "MINOF"
+
+    add_msg_and_print ('Currently in directory: %s\n' % os.getcwd())
+    add_msg_and_print ('Workspace is: %s' % arcpy.env.workspace)
+    #add_msg_and_print ('Scratch table is: %s' % out_table)
+    
+    #  get the path to the output table
+    path = os.path.dirname(out_file)
+    if len (path) == 0:
+        path = "."
+    basename = os.path.basename(out_file)
+    
+    table_view = "table_view"
+    arcmgt.MakeTableView(in_file, table_view)
+    
+    fields = arcpy.ListFields(in_file)
+    
+    rows = arcpy.SearchCursor(table_view)
+    last_row = None
+    
+    layer = "feat_layer"
+    arcmgt.MakeFeatureLayer(in_file, layer)
+    target_fld = "New_WP"
+
+    for row in rows:
+        
+        arcmgt.SelectLayerByAttribute(layer, "NEW_SELECTION", '%s = %s' % (target_fld, row.getValue(target_fld)))
+        raster = PathDistance(in_file, cost_rast)
+        
+        if last_row is None:
+            #last_row = new_row
+            continue
+        
+
+        #last_row = new_row
+    
+    fh.close
+    
+    print "Completed"
