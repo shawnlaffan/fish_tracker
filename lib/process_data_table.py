5445b7b6dd397afa9ee11f31be7ccf6b868ba097
Shawn Laffan <shawnlaffan@gmail.com>
2012-03-14 14:37:02 +1100

    Now dealing directly with feature classes, but tolerance settings are being ignored.
    Signed-off-by: Shawn Laffan <shawnlaffan@gmail.com>

--- lib/process_data_table.py
+++ lib/process_data_table.py
@@ -13,7 +13,12 @@ import string
 import math
 import pprint
 
-
+install_info = arcpy.GetInstallInfo()
+install_dir  = install_info["InstallDir"]
+default_coord_sys = os.path.join (
+    install_dir,
+    r"Coordinate Systems\Geographic Coordinate Systems\World\WGS 1984.prj"
+    )
 
 def add_msg_and_print(msg, severity=0):
     # Adds a Message to the geoprocessor (in case this is run as a tool)
@@ -46,9 +51,11 @@ if __name__ == "__main__":
 
     in_table  = arcpy.GetParameterAsText (0)
     in_table  = in_table + r"\Sheet1$"
-    out_table = arcpy.GetParameterAsText (1)
+    out_file  = arcpy.GetParameterAsText (1)
     polygon_file = arcpy.GetParameterAsText (2)
     workspace = arcpy.GetParameterAsText (3)
+
+    arcpy.env.overwriteOutput = True
 
     arcpy.env.workspace = workspace
     if (arcpy.env.workspace is None):
@@ -59,10 +66,10 @@ if __name__ == "__main__":
     add_msg_and_print ('Workspace is: %s' % arcpy.env.workspace)
     
     #  get the path to the output table
-    path = os.path.dirname(out_table)
+    path = os.path.dirname(out_file)
     if len (path) == 0:
         path = "."
-    basename = os.path.basename(out_table)
+    basename = os.path.basename(out_file)
     
     
     #  Having identified the files we now iterate over the rows in the tables
@@ -75,18 +82,11 @@ if __name__ == "__main__":
     rows = arcpy.SearchCursor(table_view)
     last_row = None
     
-    try:
-        fh = open(out_table, 'w')
-    except:
-        add_msg_and_print ("Unable to open %s for writing" % out_table)
-        raise
-
-    header  = ",".join ([("%s" % fld.name) for fld in fields])
-    header = header.replace(" ", "_")
-    header2 = ",".join ([("%s_2" % fld.name) for fld in fields])
-    header2 = header2.replace(" ", "_")
-    fh.write ("%s,%s,TIME_DIFF,TIME_DIFF_HRS\n" % (header, header2))
-
+    arcpy.env.outputCoordinateSystem = default_coord_sys
+    arcpy.env.geographicTransformations = "GDA_1994_To_WGS_1984"
+    arcpy.env.XYResolution = "0.0000000001 Meters"
+    arcpy.env.XYTolerance  = "0.0000000001 Meters"
+    
     for row in rows:
         new_row = []
         for fld in fields:
@@ -102,9 +102,28 @@ if __name__ == "__main__":
         time_diff = last_row[-1] - last_row[4]
         last_row.append (time_diff)
         last_row.append (time_diff_in_hours(time_diff))
-    
-        text = ",".join ([("%s" % k) for k in last_row])
-        fh.write (text + "\n")
+
+        pnt1 = arcpy.CreateObject("Point")
+        pnt1.X = last_row[3]
+        pnt1.Y = last_row[2]
+        pnt2 = arcpy.CreateObject("Point")
+        pnt2.X = last_row[8]
+        pnt2.Y = last_row[7]
+        line_array = arcpy.CreateObject("Array")
+        line_array.add(pnt1)
+        line_array.add(pnt2)
+        polyline = arcpy.Polyline(line_array)
+        featureList.append(polyline)
+        line_array.removeAll
+        
+        diff = ((last_row[8] - last_row[3]) ** 2 + (last_row[7] - last_row[2]) ** 2) ** 0.5
+        print "%.10f %s" % (diff, polyline.length)
 
         last_row = new_row
 
+    try:
+        arcmgt.CopyFeatures(featureList, out_file)
+        arcmgt.DefineProjection(out_file, default_coord_sys)
+        add_msg_and_print("Completed")
+    except:
+        add_msg_and_print("Unable to save to %s" % out_file)
