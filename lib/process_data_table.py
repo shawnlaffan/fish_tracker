ca1424e98f76dbd489273c4cea6a7c13cd762a90
Shawn Laffan <shawnlaffan@gmail.com>
2012-11-14 16:58:42 +1100

    Change system to create a shapefile from the input table.
    Signed-off-by: Shawn Laffan <shawnlaffan@gmail.com>

--- lib/process_data_table.py
+++ lib/process_data_table.py
@@ -13,11 +13,20 @@ import string
 import math
 import pprint
 
+#install_info = arcpy.GetInstallInfo()
+#install_dir  = install_info["InstallDir"]
+#default_coord_sys = os.path.join (
+#    install_dir,
+#    r"Coordinate Systems\Geographic Coordinate Systems\World\WGS 1984.prj"
+#    #r"Coordinate Systems\Projected Coordinate Systems\World\WGS 1984.prj"
+#    )
+default_coord_sys = arcpy.SpatialReference ("WGS 1984")
+
 def add_msg_and_print(msg, severity=0):
     # Adds a Message to the geoprocessor (in case this is run as a tool)
     # and also prints the message to the screen (standard output)
     # 
-    print msg
+    #print msg
 
     # Split the message on \n first, so that if it's multiple lines, 
     #  a GPMessage will be added for each line
@@ -41,6 +50,10 @@ def time_diff_in_hours (td):
 
 
 if __name__ == "__main__":
+    time_fld_name = "DST"
+    lat_fld_name  = "Lattitude_dd"
+    lon_fld_name  = "Longitude_dd"
+    t_diff_fld_name = "T_DIFF_HRS"
 
     in_table  = arcpy.GetParameterAsText (0)
     out_file  = arcpy.GetParameterAsText (1)
@@ -52,69 +65,80 @@ if __name__ == "__main__":
     arcpy.env.overwriteOutput = True
 
     arcpy.env.workspace = workspace
-    if (arcpy.env.workspace is None):
+    if arcpy.env.workspace is None:
         arcpy.env.workspace = os.getcwd()
-
-    out_table = out_file
 
     add_msg_and_print ('Currently in directory: %s\n' % os.getcwd())
     add_msg_and_print ('Workspace is: %s' % arcpy.env.workspace)
-    add_msg_and_print ('Scratch table is: %s' % out_table)
+    add_msg_and_print ('Output file is: %s' % out_file)
     
     #  get the path to the output table
     path = os.path.dirname(out_file)
     if len (path) == 0:
         path = "."
     basename = os.path.basename(out_file)
-    
-    try:
-        fh = open(out_table, 'w')
-    except:
-        add_msg_and_print ("Unable to open %s for writing" % out_table)
-        raise
 
-    fields = arcpy.ListFields(in_table)
-    header  = ",".join ([("%s" % fld.name) for fld in fields])
-    header = header.replace(" ", "_")
-    header2 = ",".join ([("%s_2" % fld.name) for fld in fields])
-    header2 = header2.replace(" ", "_")
-    fh.write ("%s,%s,TIME_DIFF,TIME_DIFF_HRS\n" % (header, header2))
-    
+
     #  Having identified the files we now iterate over the rows in the tables
     #  and add a number of fields
     table_view = "table_view"
     arcmgt.MakeTableView(in_table, table_view)
-    
     fields = arcpy.ListFields(in_table)
+
+    #  output stuff
+    arcpy.CreateFeatureclass_management (path, basename, "POINT")
+    out_fc_name = os.path.join (path, basename)
     
+    name_dict = {}
+    for fld in fields:
+        name = (fld.name)[0:9]
+        name_dict[fld.name] = name
+        arcpy.AddField_management(out_fc_name, name, fld.type)
+    arcpy.AddField_management(out_fc_name, t_diff_fld_name, "DOUBLE")
+
+    cur      = arcpy.InsertCursor(out_fc_name)
+    poly_pnt = arcpy.CreateObject("Point")
+
+
     rows = arcpy.SearchCursor(table_view)
     last_row = None
-    
-    featureList = []
+    prev_time = None
+    row_count = -1
+    t_diff_in_hours = 0
+
+    #featureList = []
 
     for row in rows:
-        new_row = []
-        for fld in fields:
-            new_row.append (row.getValue(fld.name))
-        
-        if last_row is None:
-            last_row = new_row
-            continue
-        
-        for item in new_row:
-            last_row.append (item)
-        #pprint.pprint (last_row)
-        time_diff = last_row[-1] - last_row[4]
-        last_row.append (time_diff)
-        last_row.append (time_diff_in_hours(time_diff))
-        
-        text = ",".join ([("%s" % k) for k in last_row])
-        print text
-        fh.write (text + "\n")
-        fh.flush
+        row_count = row_count + 1
+        this_time = row.getValue(time_fld_name)
 
-        last_row = new_row
+        if prev_time is not None:
+            time_diff = this_time - prev_time
+            t_diff_in_hours = time_diff_in_hours(time_diff)
+
+        poly_pnt.ID = row_count
+        poly_pnt.X  = row.getValue(lon_fld_name)
+        poly_pnt.Y  = row.getValue(lat_fld_name)
+
+        feat = cur.newRow()
+        feat.shape = poly_pnt
+
+        for fld in fields:
+            name = name_dict[fld.name]
+            val = row.getValue(fld.name)
+            feat.setValue(name, val)
+        feat.ID = row_count
+
+        #pprint.pprint (last_row)
+        
+        feat.setValue(t_diff_fld_name, t_diff_in_hours)
+
+        cur.insertRow(feat)
+
+        prev_time = this_time
+
+    arcpy.DefineProjection_management(out_fc_name, default_coord_sys)
     
-    fh.close
+    print arcpy.GetMessages()
     
     print "Completed"
