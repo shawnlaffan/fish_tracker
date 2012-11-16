4c9a25e44d580c7e89308acd15b2e930e43a155d
Shawn Laffan <shawnlaffan@gmail.com>
2012-11-16 16:34:04 +1100

    path residence time script seems to work now.
    Signed-off-by: Shawn Laffan <shawnlaffan@gmail.com>

--- lib/get_path_residence_time_points.py
+++ lib/get_path_residence_time_points.py
@@ -22,8 +22,13 @@ if __name__ == "__main__":
 
     in_file    = arcpy.GetParameterAsText (0)
     cost_rast  = arcpy.GetParameterAsText (1)
-    target_fld = arcpy.GetParameterAsText (2)
-    workspace  = arcpy.GetParameterAsText (3)
+    out_raster = arcpy.GetParameterAsText (2)
+    target_fld = arcpy.GetParameterAsText (3)
+    workspace  = arcpy.GetParameterAsText (4)
+    
+    if len (out_raster) == 0:
+        arcpy.AddError ("Missing argument: out_rast")
+        raise Exception
 
     if len (target_fld) == 0 or target_fld == "#":
         target_fld = "New_WP"
@@ -61,7 +66,7 @@ if __name__ == "__main__":
     arcpy.AddMessage ('Workspace is: %s' % arcpy.env.workspace)
 
     #  cumulative transits
-    transit_array_cum = arcpy.RasterToNumPyArray (CreateConstantRaster (0))
+    transit_array_accum = arcpy.RasterToNumPyArray (CreateConstantRaster (0))
 
     feat_layer = "feat_layer"
     arcmgt.MakeFeatureLayer(in_file, feat_layer)
@@ -74,11 +79,15 @@ if __name__ == "__main__":
     last_target = None
 
     for row_cur in rows:
+        transit_time = row_cur.getValue (t_diff_fld_name)
 
-        if last_target is None:
+        if last_target is None or transit_time == 0:
+            arcpy.AddMessage('Skipping record %s' % row_cur.getValue(oid_fd_name))
             last_target = row_cur.getValue(target_fld)
             last_oid    = row_cur.ID
             continue
+
+        arcpy.AddMessage ("Processing OID %i" % cur_row.getValue(oid_fd_name))
 
         arcmgt.SelectLayerByAttribute(
             feat_layer,
@@ -96,7 +105,6 @@ if __name__ == "__main__":
         path_distance = result.getOutput(0)
 
         #  get a raster of the path from origin to destination
-        #condition = '"%s" = %s or "%s" = %s' % (target_fld, last_target, target_fld, row.getValue(target_fld))
         condition = '%s in (%i, %i)' % (oid_fd_name, last_oid, row_cur.ID)
         dest_layer = "dest_layer" + str (last_oid)
         arcmgt.MakeFeatureLayer(in_file, dest_layer, where_clause = condition)
@@ -106,78 +114,66 @@ if __name__ == "__main__":
         if count == 0:
             raise NoFeatures("No features selected.  Possible coordinate system issues.\n" + condition)
 
-        path_rast = None
+        
         try:
-            path_rast = CostPath(dest_layer, path_dist_rast, backlink_rast)
-            path_dist_rast.save(arcpy.CreateScratchName("pd%d_" % last_target))
-            path_rast.save (arcpy.CreateScratchName("cp%d_" % last_target))
+            path_cost_rast = CostPath(dest_layer, path_dist_rast, backlink_rast)
+            path_cost_rast.save (arcpy.CreateScratchName("cp%d_" % last_target))
+            path_dist_rast.save (arcpy.CreateScratchName("pd%d_" % last_target))
         except Exception as e:
             raise
-            #arcpy.AddMessage (str (e))
 
         nodata = path_dist_rast.noDataValue
 
         try:
-            mask = path_rast * 0 + 1
-            mask.save (arcpy.CreateScratchName("mask%d_" % last_target))
-            xx = path_dist_rast * mask
-            path_array     = arcpy.RasterToNumPyArray(xx)
-            #path_array.clip (nodata, 0)
+            dist_masked    = path_dist_rast * (path_cost_rast * 0 + 1)
+            path_array     = arcpy.RasterToNumPyArray(dist_masked)
             path_array_idx = numpy.where(path_array != nodata)
-            #path_array      = arcpy.RasterToNumPyArray(path_rast)
             transit_array  = numpy.zeros_like(path_array)
         except:
             raise
         
 
-        pprint (path_array2)
-
-        transit_time = row_cur.getValue (t_diff_fld_name)
         path_sum = 0
-        #  loop over the array and check the neighbours
-        #  FIXME: brute force search - need to follow the path
-        #  or use numpy.where()
-        
+
         row_count = len (path_array) 
         col_count = len (path_array[0])
-        print "processing path raster, %i rows, %i cols" % (row_count, col_count)
-        i = -1
-        for row in path_array:
-            i = i + 1
-            if row.max() == nodata:  #  skip empty rows, should be faster than checking in the loop
-                continue
-            j = -1
-            for val in row:
-                j = j + 1
-                if val == nodata:
-                    continue
-                nbrs = []
-                for k in (i-1, i, i+1):
-                    if k < 0 or k >= row_count:
-                        continue    
-                    checkrow = path_array[k]
-                    for l in (j-1, j, j+1):
-                        if l < 0 or l >= col_count:
-                            continue
-                        checkval = checkrow[l]
-                        if checkval != nodata:
-                            nbrs.append(checkval)
-                minval = min (nbrs)
-                diff = val - minval
-                transit_array[i][j] = diff
-                print i, j, diff
+        print "processing %i cells of path raster" % (len(path_array_idx[0]))
+        for idx in range (len(path_array_idx[0])):
+            i = path_array_idx[0][idx]
+            j = path_array_idx[1][idx]
+            val = path_array[i][j]
+            nbrs = []
+            for k in (i-1, i, i+1):
+                if k < 0 or k >= row_count:
+                    continue    
+                checkrow = path_array[k]
+                for l in (j-1, j, j+1):
+                    if l < 0 or l >= col_count:
+                        continue
+                    checkval = checkrow[l]
+                    if checkval != nodata:
+                        nbrs.append(checkval)
+            minval = min (nbrs)
+            diff = val - minval
+            transit_array[i][j] = diff
+            #print i, j, diff
 
         path_sum = path_array.max()
         #  now calculate speed
         speed = path_sum / transit_time
-        transit_array_cum = transit_array_cum + transit_array / speed
-        print "%s, %s, %s, %s, %s" % (path_sum, transit_array.max(), speed, transit_array_cum.max(), transit_array_cum.min())
+        #  and increment the cumulative transit array
+        transit_array_accum = transit_array_accum + transit_array / speed
+        #print "%s, %s, %s, %s" % (path_sum, transit_array.max(), speed, transit_array_accum.max())
 
         #  need to use env settings to get it to be the correct size
-        xx = arcpy.NumPyArrayToRaster (transit_array_cum, lower_left_coord, cellsize_used, cellsize_used, nodata)
-        scratch = arcpy.CreateScratchName ('trans_cum_%s_' % row_cur.ID, '.img', 'raster')
-        print "Saving to %s" % scratch
-        xx.save (scratch)
+        #try:
+        #    xx = arcpy.NumPyArrayToRaster (transit_array_accum, lower_left_coord, cellsize_used, cellsize_used, nodata)
+        #    scratch = arcpy.CreateScratchName ('t_cum%s_' % row_cur.ID, None, 'raster')
+        #    print "Saving to %s" % scratch
+        #    xx.save (scratch)
+        #except:
+        #    print arcpy.GetMessages()
+        #    pass
 
         try:
             arcmgt.Delete(backlink_rast)
@@ -187,6 +183,15 @@ if __name__ == "__main__":
 
         last_target = row_cur.getValue(target_fld)
         last_oid    = row_cur.ID
+
+    #  need to use env settings to get it to be the correct size
+    try:
+        xx = arcpy.NumPyArrayToRaster (transit_array_accum, lower_left_coord, cellsize_used, cellsize_used, 0)
+        print "Saving to %s" % out_raster
+        xx.save (out_raster)
+    except:
+        raise
+
 
 
     print "Completed"
