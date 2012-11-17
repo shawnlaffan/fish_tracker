82f6a81bcee3ec9d688a53514114a2882812bea3
Shawn Laffan <shawnlaffan@gmail.com>
2012-11-17 12:04:33 +1100

    Handle spatial reference strings passed by toolbox. Replace spaces in field names with underscores.
    Signed-off-by: Shawn Laffan <shawnlaffan@gmail.com>

--- lib/table_to_shape_with_time_diff.py
+++ lib/table_to_shape_with_time_diff.py
@@ -50,7 +50,7 @@ def time_diff_in_hours (td):
 
 
 if __name__ == "__main__":
-    time_fld_name = "DST"
+    time_fld_name = "DST"           #  FIXME:  make these arguments
     lat_fld_name  = "Lattitude_dd"
     lon_fld_name  = "Longitude_dd"
     t_diff_fld_name = "T_DIFF_HRS"
@@ -63,20 +63,29 @@ if __name__ == "__main__":
     if string.count(in_table, 'xls') and not string.count(in_table, 'Sheet1$'):
         in_table  = in_table + r"\Sheet1$"
 
-    arcpy.env.overwriteOutput = True
+    #arcpy.env.overwriteOutput = True
 
     arcpy.env.workspace = workspace
     if arcpy.env.workspace is None:
         arcpy.env.workspace = os.getcwd()
-
-    if len(coord_sys) == 0:
+    
+    arcpy.AddMessage ("Coordinate sys is %s" % arcpy.env.outputCoordinateSystem)
+    arcpy.AddMessage ("coord_sys arg is %s" % coord_sys)
+    if len(coord_sys) == 0 and arcpy.env.outputCoordinateSystem is None:
         coord_sys = default_coord_sys
     else:
-        coord_sys = arcpy.SpatialReference (coord_sys)
+        try:  #  is it a spatial ref string?
+            sr = arcpy.SpatialReference()
+            sr.loadFromString (coord_sys)
+            coord_sys = sr.name
+        except Exception as e:
+            arcpy.AddMessage (e)
+            coord_sys = arcpy.SpatialReference (coord_sys)
 
     arcpy.AddMessage ('Currently in directory: %s\n' % os.getcwd())
     arcpy.AddMessage ('Workspace is: %s' % arcpy.env.workspace)
     arcpy.AddMessage ('Output file is: %s' % out_file)
+    arcpy.AddMessage ('Coord sys for result is %s' % coord_sys)
 
     #  get the path to the output table
     path = os.path.dirname(out_file)
@@ -98,6 +107,7 @@ if __name__ == "__main__":
     name_dict = {}
     for fld in fields:
         name = (fld.name)[0:9]
+        name = name.replace (" ", "_")
         name_dict[fld.name] = name
         arcpy.AddField_management(out_fc_name, name, fld.type)
     arcpy.AddField_management(out_fc_name, t_diff_fld_name, "DOUBLE")
@@ -127,9 +137,12 @@ if __name__ == "__main__":
         feat = cur.newRow()
         feat.shape = poly_pnt
 
+        arcpy.AddMessage ("t_diff_fld_name = %s, t_diff_in_hours = %s" % (t_diff_fld_name, t_diff_in_hours))
+
         for fld in fields:
             name = name_dict[fld.name]
             val = row.getValue(fld.name)
+            arcpy.AddMessage ("name = %s, val = %s" % (name, val))
             feat.setValue(name, val)
         feat.ID = row_count
 
